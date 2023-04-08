891f7f82c78e2126d8751732bc3d6a98863a4173
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-04-08 12:51:56 -0300

    FT002: Feat - Create AmountInput properties, event emitter and markup

--- src/app/shared/components/amount-input/amount-input.component.ts
+++ src/app/shared/components/amount-input/amount-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-amount-input',
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./amount-input.component.scss']
 })
 export class AmountInputComponent {
+  @Input() amount: number = 0;
+  @Input() min: number | null = null;
+  @Input() max: number | null = null;
+  
+  @Output() onChange = new EventEmitter<number>();
 
+  handleClick(action: 'sum' | 'sub') {
+    if (action === 'sum') {
+      if (this.max !== null && this.amount >= this.max) {
+          return;
+      }
+
+      this.amount++;
+      this.emit()
+    } else {
+      if (this.min !== null && this.amount <= this.min) {
+          return;
+      }
+
+      this.amount--;
+      this.emit()
+    }
+  }
+
+  emit() {
+    this.onChange.emit(this.amount);
+  }
 }
