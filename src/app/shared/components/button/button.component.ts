4f2a74f828c66de336279b01a6a232a6d2d0c1bd
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-04-10 18:17:09 -0300

    FT002: Feat - Create Button properties, event emitter and markup

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./button.component.scss']
 })
 export class ButtonComponent {
+  @Input() disabled = false;
+  @Output() click = new EventEmitter();
 
+  handleClick(event: any) {
+    this.click.emit(event);
+  }
 }
