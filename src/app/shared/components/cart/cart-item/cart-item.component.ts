a7ab4a19a56f730acbcd19b091007f90c6ad1494
Igor José Castro Mariano Lima <igormariano@outlook.com.br>
2023-04-16 22:01:48 -0300

    FT003: Feat - Show CartItem in Cart component

--- src/app/shared/components/cart/cart-item/cart-item.component.ts
+++ src/app/shared/components/cart/cart-item/cart-item.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { CartStore } from 'src/app/core/stores/cart/cart.store';
+import { Product } from 'src/app/core/models/product';
+import { Store } from '@ngrx/store';
+import { CartActions } from 'src/app/core/stores/cart/cart.actions';
 
 @Component({
   selector: 'app-cart-item',
@@ -6,5 +12,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./cart-item.component.scss']
 })
 export class CartItemComponent {
+  @Input() product: Product;
+  cart$: Observable<CartStore>;
 
+  constructor(
+    private store: Store<{ cart: CartStore }>
+  ) {
+    this.product = {} as Product;
+    this.cart$ = this.store.select('cart');
+  }
+
+  onRemoveFromCart(product: Product) {
+    this.store.dispatch(
+      CartActions.remove_product({ product })
+    );
+  }
 }
